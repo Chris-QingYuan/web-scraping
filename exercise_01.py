7c09c83577e211af3fc639220567db2be2d6c415
Chris-QingYuan <chris453442895@gmail.com>
2018-03-03 16:08:51 -0500

    Initial commit

---
+++ exercise_01.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as ureq
+
+target_url = "https://www.youtube.com/watch?v=XQgXKtPSzUI"
+
+uClient = ureq(target_url)
+html = uClient.read()
+uClinet.close()
+
+page_soup = bs(html, "html.parser")
+
+print(page_soup.h1)
